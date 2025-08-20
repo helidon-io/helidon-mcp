7e93123606f463305472af9fcc837902c2ae7008
tvallin <thibault.vallin@oracle.com>
2025-08-20 22:15:31 +0200

    Fix windows logging failing test

--- tests/declarative/src/test/java/io/helidon/extensions/mcp/tests/declarative/AnthropicLoggingTest.java
+++ tests/declarative/src/test/java/io/helidon/extensions/mcp/tests/declarative/AnthropicLoggingTest.java
@@ -20,6 +20,7 @@ import java.time.Duration;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 import io.helidon.webserver.WebServer;
 import io.helidon.webserver.testing.junit5.ServerTest;
@@ -37,8 +38,8 @@ import static org.hamcrest.Matchers.is;
 
 @ServerTest
 class AnthropicLoggingTest {
-    private static final CountDownLatch LATCH = new CountDownLatch(6);
     private static McpSyncClient client;
+    private CountDownLatch latch;
 
     AnthropicLoggingTest(WebServer server) {
         client = McpClient.sync(HttpClientSseClientTransport.builder("http://localhost:" + server.port())
@@ -48,7 +49,7 @@ class AnthropicLoggingTest {
                     assertThat(notification.data(), is("Logging notification"));
                     assertThat(notification.logger(), is("helidon-logger"));
                     assertThat(notification.level(), is(McpSchema.LoggingLevel.INFO));
-                    LATCH.countDown();
+                    latch.countDown();
                 })
                 .requestTimeout(Duration.ofSeconds(1))
                 .build();
@@ -64,43 +65,43 @@ class AnthropicLoggingTest {
 
     @ParameterizedTest
     @ValueSource(strings = {"loggingTool", "loggerTool"})
-    void testLoggingTool(String name) {
-        long current = LATCH.getCount();
+    void testLoggingTool(String name) throws InterruptedException {
+        latch = new CountDownLatch(1);
         var result = client.callTool(new McpSchema.CallToolRequest(name, Map.of()));
+        assertThat(latch.await(1, TimeUnit.SECONDS), is(true));
 
         List<McpSchema.Content> contents = result.content();
         assertThat(contents.size(), is(1));
 
         McpSchema.TextContent text = (McpSchema.TextContent) contents.getFirst();
         assertThat(text.text(), is("Hello World"));
-        assertThat(LATCH.getCount(), is(current - 1));
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"loggingPrompt", "loggerPrompt"})
-    void testLoggingPrompt(String name) {
-        long current = LATCH.getCount();
+    void testLoggingPrompt(String name) throws InterruptedException {
+        latch = new CountDownLatch(1);
         var result = client.getPrompt(new McpSchema.GetPromptRequest(name, Map.of()));
+        assertThat(latch.await(1, TimeUnit.SECONDS), is(true));
 
         var contents = result.messages();
         assertThat(contents.size(), is(1));
 
         var text = (McpSchema.TextContent) contents.getFirst().content();
         assertThat(text.text(), is("Hello World"));
-        assertThat(LATCH.getCount(), is(current - 1));
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"file://hello/world", "file://hello/world1"})
-    void testLoggingResource(String uri) {
-        long current = LATCH.getCount();
+    void testLoggingResource(String uri) throws InterruptedException {
+        latch = new CountDownLatch(1);
         var result = client.readResource(new McpSchema.ReadResourceRequest(uri));
+        assertThat(latch.await(1, TimeUnit.SECONDS), is(true));
 
         var contents = result.contents();
         assertThat(contents.size(), is(1));
 
         var text = (McpSchema.TextResourceContents) contents.getFirst();
         assertThat(text.text(), is("Hello World"));
-        assertThat(LATCH.getCount(), is(current - 1));
     }
 }
